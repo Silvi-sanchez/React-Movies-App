fdc98ff0fbd72e26bea1a1a21e423a340a9e5684
Raquel Muriega <muriega.raquel@gmail.com>
2021-07-28 20:01:38 -0300

    Paginado terminado

---
+++ src/components/Pagination/CustomPagination.js
@@ -0,0 +1,36 @@
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
+import './CustomPagination.css';
+
+
+const darkTheme = createMuiTheme({
+    palette:{
+        type:'dark'
+    }
+})
+
+
+const customPagination = ({setPage, numOfPages = 50}) => {
+    const handlePageChange = (page) =>{
+        setPage(page);
+        window.scroll(0,0);
+    };
+
+    return (
+    <div>
+        <ThemeProvider theme={darkTheme}>
+            <Pagination 
+            count={numOfPages}
+            onChange = {(e) => handlePageChange(e.target.textContent)}
+            variant="outlined"
+            shape="rounded"
+            color="primary"
+            // hideNextButton
+            // hidePrevButton
+            />
+        </ThemeProvider>
+    </div>
+    );
+};
+
+export default customPagination;
