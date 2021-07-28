ab198cd1b81d04c404ba82593ab43b989585b77c
Silvi <silvinans@gmail.com>
2021-07-28 19:24:08 -0300

    Cambios pag. principal

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import useFetch from './hooks/useFetch';
+// import { URL_API, REACT_APP_API_KEY } from '.env';
+import { Row, Col } from 'antd';
 
+import SliderMovies from './SliderMovies';
+import MovieList from './MovieList';
+import Footer from './Footer';
+
+const REACT_APP_API_KEY = '9c0cae2349f71e57d0b415386e382c68'
+const URL_API = 'https://api.themoviedb.org/3'
 
 const Home = () => {
+
+    const url = `${URL_API}/movie/now_playing?api_key=${REACT_APP_API_KEY}&language=en-ES&page=1`
+    const urlPopular = `${URL_API}/movie/popular?api_key=${REACT_APP_API_KEY}&language=en-ES&page=1`
+    const urlRanked = `${URL_API}/movie/top_rated?api_key=${REACT_APP_API_KEY}&language=en-ES&page=1`
+   
+    const newMovies = useFetch(url);
+    const popularMovies = useFetch(urlPopular);
+    const topRankedMovies = useFetch(urlRanked);
+
+
     return (
         <div>
-        <span className="pageTitle">Home</span>
-
+            <SliderMovies movies={newMovies} />
+            {/* <Row>
+                <Col span={12}>
+                    <MovieList title="Películas Populares" movies={popularMovies} />
+                </Col>
+                <Col span={12}>
+                    <MovieList title="Top Mejores Películas" movies={topRankedMovies} />
+                </Col>
+            </Row> */}
+            <Footer />
         </div>
     )
 }
